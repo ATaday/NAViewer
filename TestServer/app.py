28da714112c2f3bf78724013160fff9ba1dd3e07
Alper Taday <trshield@gmail.com>
2014-07-08 17:55:32 +0300

    Update app.py

--- TestServer/app.py
+++ TestServer/app.py
@@ -33,7 +33,7 @@ def saveData():
 def index():
 
     activity = mongo.db.netactivity.find() 
-    size_total = 0 
+
     html = '<table width="80%"><td><u><b>Time</b></u></td><td><u><b>Protocol</b></u></td><td><u><b>Source Address</b></u></td><td><u><b>Destination Address</b></u></td><td><u><b>Destination Port</b></u></td><td><u><b>Packet Size</b></u></td>'
 
     for record in activity:
@@ -53,26 +53,30 @@ def index():
 
 @app.route('/length', methods=['GET'])
 
-def size():
+def size():	
+    
+    html = '<table width="80%"><td><u><b>Packet Size</b></u></td>'
 
     activity = mongo.db.netactivity.find() 
-
-    size_total = 0 
-	
-    html = '<table width="80%"><td><u><b>Total Size</b></u></td><td><u><b>Source Address</b></u></td><td><u><b>Destination Port</b></u></td>'
-
-    for record in activity:
-        html = html + '<tr>' 
-	size_total = size_total + record['packet size']
-    	html = html + '<td>%s</td>' % size_total
-        html = html + '<td>%s</td>' % record['source address']
-        html = html + '<td>%s</td>' % record['destination port']
-        html = html + '</tr>'
+    	
+    packet_size = mongo.db.netactivity.aggregate([ { 
+    '$group': { 
+        '_id': 'null', 
+        'total': { 
+            '$sum': "$packet size" 
+        }
+    } 
+} ] )
+    
+    html = html + '<tr>' 
+    html = html + '<td>%s</td>' % packet_size # fix the line for direct view of total length 
+    html = html + '</tr>'
     html = html + '</table>'
 
     response = html
 
     return response
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
