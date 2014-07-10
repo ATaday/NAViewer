1391c1cef6ee8145f9c6b0f715341eef589e3a5f
Alper Taday <trshield@gmail.com>
2014-07-10 12:52:50 +0300

    Update app.py

--- TestServer/app.py
+++ TestServer/app.py
@@ -30,9 +30,9 @@ def saveData():
 
 @app.route('/view', methods=['GET'])
 
-def index():
+def view():
 
-    activity = mongo.db.netactivity.find() 
+    activity = mongo.db.netactivity.find()
 
     html = '<table width="80%"><td><u><b>Time</b></u></td><td><u><b>Protocol</b></u></td><td><u><b>Source Address</b></u></td><td><u><b>Destination Address</b></u></td><td><u><b>Destination Port</b></u></td><td><u><b>Packet Size</b></u></td>'
 
@@ -51,25 +51,47 @@ def index():
 
     return response
 
-@app.route('/length', methods=['GET'])
+@app.route('/size_sort', methods=['GET'])
 
-def size():	
+def size_sort():
+
+    activity = mongo.db.netactivity.find({ 'packet size':{"$gt": 1	}}).sort( [('packet size', -1) ] )
+
+    html = '<table width="80%"><td><u><b>Time</b></u></td><td><u><b>Protocol</b></u></td><td><u><b>Source Address</b></u></td><td><u><b>Destination Address</b></u></td><td><u><b>Destination Port</b></u></td><td><u><b>Packet Size</b></u></td>'
+
+    for record in activity:
+        html = html + '<tr>' 
+	html = html + '<td>%s</td>' % record['time']
+	html = html + '<td>%s</td>' % record['protocol']
+	html = html + '<td>%s</td>' % record['source address']
+        html = html + '<td>%s</td>' % record['destination address']
+	html = html + '<td>%s</td>' % record['destination port']
+        html = html + '<td>%s</td>' % record['packet size']
+        html = html + '</tr>'
+    html = html + '</table>'
+
+    response = html
+
+    return response
+
+
+@app.route('/saddr_sort', methods=['GET'])
+
+def saddr_sort():	
     
     html = '<table width="80%"><td><u><b>Packet Size</b></u></td>'
-
-    activity = mongo.db.netactivity.find() 
     	
     packet_size = mongo.db.netactivity.aggregate([ { 
     '$group': { 
-        '_id': 'null', 
-        'total': { 
+        '_id': '$source address', 
+        'Total Size': { 
             '$sum': "$packet size" 
         }
     } 
-} ] )
+}, { '$sort': { 'Total Size': -1 } }] )
     
     html = html + '<tr>' 
-    html = html + '<td>%s</td>' % packet_size # fix the line for direct view of total length 
+    html = html + '<td>%s</td>' % packet_size
     html = html + '</tr>'
     html = html + '</table>'
 
@@ -77,6 +99,53 @@ def size():
 
     return response
 
+@app.route('/daddr_sort', methods=['GET'])
+
+def daddr_sort():	
+    
+    html = '<table width="80%"><td><u><b>Packet Size</b></u></td>'
+    	
+    packet_size = mongo.db.netactivity.aggregate([ { 
+    '$group': { 
+        '_id': '$destination address', 
+        'Total Size': { 
+            '$sum': "$packet size" 
+        }
+    } 
+}, { '$sort': { 'Total Size': -1 } }] )
+    
+    html = html + '<tr>' 
+    html = html + '<td>%s</td>' % packet_size
+    html = html + '</tr>'
+    html = html + '</table>'
+
+    response = html
+
+    return response
+
+@app.route('/dport_sort', methods=['GET'])
+
+def dport_sort():	
+    
+    html = '<table width="80%"><td><u><b>Packet Size</b></u></td>'
+    	
+    packet_size = mongo.db.netactivity.aggregate([ { 
+    '$group': { 
+        '_id': '$destination port', 
+        'Total Size': { 
+            '$sum': "$packet size" 
+        }
+    } 
+}, { '$sort': { 'Total Size': -1 } }] )
+    
+    html = html + '<tr>' 
+    html = html + '<td>%s</td>' % packet_size
+    html = html + '</tr>'
+    html = html + '</table>'
+
+    response = html
+
+    return response
 
 if __name__ == '__main__':
 	app.run(debug=True)
