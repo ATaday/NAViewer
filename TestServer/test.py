983fb1f8950309b69872d306019a4f4c35e7da8b
Alper Taday <trshield@gmail.com>
2014-07-11 16:52:26 +0300

    Update test.py

--- TestServer/test.py
+++ TestServer/test.py
@@ -6,8 +6,6 @@ from receivedata import receiveData
 while True:
 
     data = receiveData()
-
-    # TODO : filter post content from logs.
     r = requests.post('http://127.0.0.1:5000', data=json.dumps(data))
 
     print r.text
