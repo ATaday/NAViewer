1bca87923b55ff17dd7ca4de5acb870a601bb1a9
Alper Taday <trshield@gmail.com>
2014-07-17 14:57:59 +0300

    Update app.py

--- TestServer/app.py
+++ TestServer/app.py
@@ -48,7 +48,7 @@ def view():
 
 def specific_time():
 
-    activity = mongo.db.netactivity.find( { 'time': { "$gt": 1405434044.88 , "$lt": 1405434167.98 } } )
+    activity = mongo.db.netactivity.find( { 'time': { "$gt": 1405520605.82 , "$lt": 1405520614.3 } } )
     
     log = [record for record in activity]
 
@@ -64,9 +64,11 @@ def size_sort():
   
     t_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': 'null', 'Total Size': { '$sum': "$packet size" } } } ] )
     
+    log_per = [record_per for record_per in t_size['result']] 
+ 
     log = [record for record in activity]    
 
-    return render_template('size_sort.html', records=log)
+    return render_template('size_sort.html', records=log, records_per=log_per)
 
 # total packet size of all logs
 
@@ -86,11 +88,15 @@ def total_size():
 
 def saddr_size():	
     	    
-    packet_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': '$source address', 'Total Size': { '$sum': "$packet size" } } } , { '$sort': { 'Total Size': -1 } } ] )
+    packet_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': '$source address', 'SAddr Size': { '$sum': "$packet size" } } } , { '$sort': { 'SAddr Size': -1 } } ] )
+
+    t_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': 'null', 'Total Size': { '$sum': "$packet size" } } } ] )
+    
+    log_per = [record_per for record_per in t_size['result']] 
 
     log = [record for record in packet_size['result']]    
 
-    return render_template('saddr_size.html', records=log)
+    return render_template('saddr_size.html', records=log, records_per=log_per)
 
 # sorting packet size of destination address
 
@@ -98,11 +104,15 @@ def saddr_size():
 
 def daddr_size():	
    
-    packet_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': '$destination address', 'Total Size': { '$sum': "$packet size" } } } , { '$sort': { 'Total Size': -1 } } ] )
+    packet_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': '$destination address', 'DAddr Size': { '$sum': "$packet size" } } } , { '$sort': { 'DAddr Size': -1 } } ] )
+
+    t_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': 'null', 'Total Size': { '$sum': "$packet size" } } } ] )
+    
+    log_per = [record_per for record_per in t_size['result']] 
 
     log = [record for record in packet_size['result']]    
 
-    return render_template('daddr_size.html', records=log)   
+    return render_template('daddr_size.html', records=log, records_per=log_per)   
 
 # sorting packet size of destination port
 
@@ -110,11 +120,15 @@ def daddr_size():
 
 def dport_size():	
     	
-    packet_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': '$destination port', 'Total Size': { '$sum': "$packet size" } } } , { '$sort': { 'Total Size': -1 } } ] )
+    packet_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': '$destination port', 'DPort Size': { '$sum': "$packet size" } } } , { '$sort': { 'DPort Size': -1 } } ] )
       
+    t_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': 'null', 'Total Size': { '$sum': "$packet size" } } } ] )
+    
+    log_per = [record_per for record_per in t_size['result']] 
+
     log = [record for record in packet_size['result']]    
 
-    return render_template('dport_size.html', records=log)
+    return render_template('dport_size.html', records=log, records_per=log_per)
 
 # total average size of all logs
 
@@ -122,9 +136,9 @@ def dport_size():
 
 def total_avg():	
     	
-    packet_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': 'null', 'Total Average': { '$avg': "$packet size" } } } ] )
+    t_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': 'null', 'Total Average': { '$avg': "$packet size" } } } ] )
     
-    log = [record for record in packet_size['result']]    
+    log = [record for record in t_size['result']]    
 
     return render_template('total_avg.html', records=log)
 
@@ -134,11 +148,15 @@ def total_avg():
 
 def saddr_avg():	
 
-    packet_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': '$source address', 'Total Average': { '$avg': "$packet size" } } } , { '$sort': { 'Total Average': -1 } } ] )
+    packet_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': '$source address', 'SAddr Average': { '$avg': "$packet size" } } } , { '$sort': { 'SAddr Average': -1 } } ] )
+
+    t_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': 'null', 'Total Average': { '$avg': "$packet size" } } } ] )
+
+    log_per = [record_per for record_per in t_size['result']] 
 
     log = [record for record in packet_size['result']]    
 
-    return render_template('saddr_avg.html', records=log)
+    return render_template('saddr_avg.html', records=log, records_per=log_per)
 
 # sorting average size of destination address
 
@@ -146,11 +164,15 @@ def saddr_avg():
 
 def daddr_avg():	
 
-    packet_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': '$destination address', 'Total Average': { '$avg': "$packet size" } } } , { '$sort': { 'Total Average': -1 } } ] )
+    packet_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': '$destination address', 'DAddr Average': { '$avg': "$packet size" } } } , { '$sort': { 'DAddr Average': -1 } } ] )
+
+    t_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': 'null', 'Total Average': { '$avg': "$packet size" } } } ] )
+
+    log_per = [record_per for record_per in t_size['result']] 
     
     log = [record for record in packet_size['result']]    
 
-    return render_template('daddr_avg.html', records=log)
+    return render_template('daddr_avg.html', records=log, records_per=log_per)
 
 # sorting average size of destination port
 
@@ -158,11 +180,15 @@ def daddr_avg():
 
 def dport_avg():	
  	
-    packet_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': '$destination port', 'Total Average': { '$avg': "$packet size" } } } , { '$sort': { 'Total Average': -1 } } ] )
+    packet_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': '$destination port', 'DPort Average': { '$avg': "$packet size" } } } , { '$sort': { 'DPort Average': -1 } } ] )
     
+    t_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': 'null', 'Total Average': { '$avg': "$packet size" } } } ] )
+
+    log_per = [record_per for record_per in t_size['result']] 
+
     log = [record for record in packet_size['result']]    
 
-    return render_template('dport_avg.html', records=log)
+    return render_template('dport_avg.html', records=log, records_per=log_per)
 
 # total count of all logs
 
@@ -170,9 +196,9 @@ def dport_avg():
 
 def total_count():	
 
-    packet_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': 'null', 'Total Count': { '$sum': 1 } } } ] )
+    t_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': 'null', 'Total Count': { '$sum': 1 } } } ] )
     
-    log = [record for record in packet_size['result']]    
+    log = [record for record in t_size['result']]    
 
     return render_template('total_count.html', records=log)
 
@@ -182,11 +208,15 @@ def total_count():
 
 def saddr_count():	
 
-    packet_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': '$source address', 'Total Count': { '$sum': 1 } } } , { '$sort': { 'Total Count': -1 } } ] )
+    packet_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': '$source address', 'SAddr Count': { '$sum': 1 } } } , { '$sort': { 'SAddr Count': -1 } } ] )
+
+    t_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': 'null', 'Total Count': { '$sum': 1 } } } ] )
+
+    log_per = [record_per for record_per in t_size['result']]  
 
     log = [record for record in packet_size['result']]    
 
-    return render_template('saddr_count.html', records=log)
+    return render_template('saddr_count.html', records=log, records_per=log_per)
 
 # sorting count of destination address
 
@@ -194,11 +224,15 @@ def saddr_count():
 
 def daddr_count():	
 
-    packet_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': '$destination address', 'Total Count': { '$sum': 1 } } } , { '$sort': { 'Total Count': -1 } } ] )
+    packet_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': '$destination address', 'DAddr Count': { '$sum': 1 } } } , { '$sort': { 'DAddr Count': -1 } } ] )
+
+    t_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': 'null', 'Total Count': { '$sum': 1 } } } ] )
+
+    log_per = [record_per for record_per in t_size['result']]  
 
     log = [record for record in packet_size['result']]    
 
-    return render_template('daddr_count.html', records=log)
+    return render_template('daddr_count.html', records=log, records_per=log_per)
 
 # sorting count of destination port
 
@@ -206,11 +240,15 @@ def daddr_count():
 
 def dport_count():	
 
-    packet_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': '$destination port', 'Total Count': { '$sum': 1 } } } , { '$sort': { 'Total Count': -1 } } ] )
+    packet_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': '$destination port', 'DPort Count': { '$sum': 1 } } } , { '$sort': { 'DPort Count': -1 } } ] )
+
+    t_size = mongo.db.netactivity.aggregate( [ { '$group': { '_id': 'null', 'Total Count': { '$sum': 1 } } } ] )
+
+    log_per = [record_per for record_per in t_size['result']]  
 
     log = [record for record in packet_size['result']]    
 
-    return render_template('dport_count.html', records=log)
+    return render_template('dport_count.html', records=log, records_per=log_per)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
